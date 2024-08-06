529671d33329fa90ff8da8fa632f364f8eb8fe0e
supersanta183 <klemmensen11@gmail.com>
2024-08-06 17:32:36 +0200

    updates for fast miner

--- src/send_and_confirm.rs
+++ src/send_and_confirm.rs
@@ -27,7 +27,7 @@ const _SIMULATION_RETRIES: usize = 4;
 const GATEWAY_RETRIES: usize = 150;
 const CONFIRM_RETRIES: usize = 1;
 
-const CONFIRM_DELAY: u64 = 1000;
+const CONFIRM_DELAY: u64 = 0;
 const GATEWAY_DELAY: u64 = 500;
 
 pub enum ComputeBudget {
